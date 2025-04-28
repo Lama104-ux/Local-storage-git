e21f7acf2b85e482ca48fdf0b444c18e046a539f
Lama <lamamamoun90@gmail.com>
2025-04-28 11:38:53 +0200

    render mocked post images on screen

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 window.addEventListener("DOMContentLoaded", main);
+const posts = [{
+    id: 1,
+    title: "A cute cat",
+    imageUrl: "https://images.pexels.com/photos/29806036/pexels-photo-29806036.jpeg?cs=srgb&dl=pexels-henkephotoart-29806036.jpg&fm=jpg&_gl=1*vmjvh9*_ga*NTc1OTI4NzI4LjE3NDU4MzIyMDA.*_ga_8JE65Q40S6*MTc0NTgzMjIwMC4xLjEuMTc0NTgzMjIxNy4wLjAuMA..",
+
+},
+{
+    id: 2,
+    title: "A couple",
+    imageUrl: "https://images.pexels.com/photos/6550335/pexels-photo-6550335.jpeg",
+
+},
+{
+    id: 3,
+    title: "A nice car",
+    imageUrl: "https://images.pexels.com/photos/18765/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+
+},
+];
 
 function main() {
     //what should the program do at start?
+    renderPosts();
+}
+function renderPosts() {
+
+    const main = document.querySelector("main");
+    for (const post of posts) {
+        const img = document.createElement("img");
+        img.src = post.imageUrl;
+        img.alt = post.title;
+        img.className = "image";// för css
+        main.append(img);
+    }
 }
